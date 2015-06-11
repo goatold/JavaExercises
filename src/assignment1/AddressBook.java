92235e32774cd0b5204b54a8dd53a765e07728f3
leow <goatold@gmail.com>
2015-06-11 17:08:37 +0800

    complete func compareNames

--- src/assignment1/AddressBook.java
+++ src/assignment1/AddressBook.java
@@ -85,11 +85,62 @@ public class AddressBook {
 	}
 
 	/**
+	 * @param fullname
+	 * @param pattern: format of fullname
+	 * @param lastName
+	 */
+	public AddressBook(String fullName, Pattern pattern) {
+		super();
+		Matcher matcher = pattern.matcher(fullName);
+		if (matcher.matches()) {
+			this.firstName = matcher.group(1);
+			this.middleName = matcher.group(2);
+			this.lastName = matcher.group(3);
+		} else {
+			System.out.println("invalid name: " + fullName);
+		}
+	}
+
+	/**
+	 * @param name1, name2: 2 name strings to compare
+	 * return String describe the comparison result
+	 */
+	public static String compareNames(String name1, String name2) {
+		String result = "";
+		AddressBook ab1, ab2;
+		Pattern pattern = Pattern.compile("(\\w+)\\s+(\\w+\\.)\\s+(\\w+)");
+		ab1 = new AddressBook(name1, pattern);
+		ab2 = new AddressBook(name2, pattern);
+		result += "firstnames "
+				+ ab1.getFirstName()
+				+ " and "
+				+ ab2.getFirstName()
+				+ " are "
+				+ (ab1.getFirstName().equals(ab2.getFirstName()) ? "the same"
+						: "defferent") + "\n";
+		result += "midnames "
+				+ ab1.getMiddleName()
+				+ " and "
+				+ ab2.getMiddleName()
+				+ " are "
+				+ (ab1.getMiddleName().equals(ab2.getMiddleName()) ? "the same"
+						: "defferent") + "\n";
+		result += "lastnames "
+				+ ab1.getLastName()
+				+ " and "
+				+ ab2.getLastName()
+				+ " are "
+				+ (ab1.getLastName().equals(ab2.getLastName()) ? "the same"
+						: "defferent") + "\n";
+		return result;
+	}
+	
+	/**
 	 * @param numStr a string represent a phone number to be validated
 	 * return boolean
 	 */
 	public static boolean validPhoneNum(String numStr) {
-		Pattern pattern = Pattern.compile("+?\\d+[\\d\\- ]*\\d");
+		Pattern pattern = Pattern.compile("^+?\\d+[\\d\\- ]*\\d$");
 		Matcher matcher = pattern.matcher(numStr);
 		return matcher.matches();
 	}
@@ -99,8 +150,11 @@ public class AddressBook {
 	 */
 	public static void main(String[] args) {
 		// test this class
-		
-
+		System.out.println(AddressBook.compareNames("Xman X. Good", "Xman XY. Well"));
+		System.out.println(AddressBook.compareNames("Xman X. Good", "Xman Bad"));
+		AddressBook abtest = new AddressBook("xx", "yy", "zz");
+		abtest.setBusinessPhone("sss54354");
+		abtest.setBusinessPhone("111-999-334");
 	}
 
 	/**
